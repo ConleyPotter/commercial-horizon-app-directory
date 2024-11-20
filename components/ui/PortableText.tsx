a092217f413e399325d40be8f1ba15f590d674d9
ConleyPotter <conley.potter@gmail.com>
2024-11-20 13:06:15 -0500

    Enhance documentation for PortableText component and its custom components

--- components/ui/PortableText.tsx
+++ components/ui/PortableText.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/Button";
 import { PortableText, PortableTextComponents, PortableTextProps } from "@portabletext/react";
 import Link from "next/link";
-// import { IdealImage } from "./ImageWrapper";
-// import { SanityAssetSourceData } from "@/sanity.types";
 import { TypedObject } from "sanity";
 
+/**
+ * Props for the CallToAction component.
+ * 
+ * @property {Object} value - The value object containing the call to action details.
+ * @property {string} value.ctaCopy - The copy text for the call to action.
+ * @property {string} value.link - The URL link for the call to action.
+ * @property {boolean} isInline - Determines if the call to action should be displayed inline.
+ */
 interface CallToActionProps {
   value: {
     ctaCopy: string;
@@ -13,23 +19,36 @@ interface CallToActionProps {
   isInline: boolean;
 }
 
-// interface ImageProps {
-//   value: {
-//     url: string;
-//     source: SanityAssetSourceData;
-//     altText: string;
-//   };
-// }
-
+/**
+ * Custom components for rendering Portable Text content.
+ * 
+ * @constant
+ * @type {Object}
+ * @property {Object} types - Custom components for specific block types.
+ * @property {Function} types.cta - Renders a call-to-action (CTA) component.
+ * @property {Object} marks - Custom components for inline marks.
+ * @property {Function} marks.link - Renders a link component.
+ * 
+ * @example
+ * // Example usage of the customPortableTextComponents
+ * <PortableText
+ *   value={content}
+ *   components={customPortableTextComponents}
+ * />
+ * 
+ * @typedef {Object} CallToActionProps
+ * @property {Object} value - The value object containing CTA details.
+ * @property {string} value.link - The URL for the CTA link.
+ * @property {string} value.ctaCopy - The text to display for the CTA.
+ * @property {boolean} isInline - Indicates if the CTA should be rendered inline.
+ * 
+ * @typedef {Object} LinkProps
+ * @property {Object} value - The value object containing link details.
+ * @property {string} value.href - The URL for the link.
+ * @property {React.ReactNode} children - The content to be wrapped by the link.
+ */
 const customPortableTextComponents = {
   types: {
-        // image: ({ value }: ImageProps) => (
-    //   <IdealImage src={value.url} alt={value.altText} image={{
-    //     url: value.url,
-    //     source: value.source,
-    //     altText: value.altText,
-    //   }} />
-    // ),
     cta: ({ value, isInline }: CallToActionProps) => 
       isInline ? (
         <a href={value.link} className="text-primary underline-offset-4 hover:underline">{value.ctaCopy}</a>
@@ -51,10 +70,24 @@ const customPortableTextComponents = {
   },
 }
 
+/**
+ * Props for the CustomPortableText component.
+ *
+ * @interface CustomPortableTextProps
+ * @property {TypedObject | TypedObject[]} value - The value to be rendered by the PortableText component. 
+ * This can be a single TypedObject or an array of TypedObjects.
+ */
 interface CustomPortableTextProps {
   value: TypedObject | TypedObject[];
 }
 
+/**
+ * CustomPortableText component renders a PortableText component with custom components.
+ *
+ * @param {CustomPortableTextProps} props - The properties for the CustomPortableText component.
+ * @param {any} props.value - The value to be rendered by the PortableText component.
+ * @returns {JSX.Element} The rendered PortableText component with custom components.
+ */
 export const CustomPortableText = (props: CustomPortableTextProps) => {
   return <PortableText value={props.value} components={customPortableTextComponents as PortableTextComponents} />
 }
