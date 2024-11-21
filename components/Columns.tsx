dfea872d4b517a9653d4473ae45c77e7ac690ec7
ConleyPotter <conley.potter@gmail.com>
2024-11-21 08:11:05 -0500

    Add TypeScript documentation for Columns component props and functionality

--- components/Columns.tsx
+++ components/Columns.tsx
@@ -3,10 +3,27 @@ import { v4 as uuid } from 'uuid';
 import { ColumnsType } from "@/lib/types";
 import { IdealImage } from "./ui/ImageWrapper";
 
+/**
+ * Props for the Columns component.
+ * 
+ * @interface ColumnsProps
+ * @property {ColumnsType} twoColumns - Defines the structure and content of the two columns.
+ */
 interface ColumnsProps {
     twoColumns: ColumnsType
 }
 
+/**
+ * A React functional component that renders two columns of content blocks.
+ *
+ * @component
+ * @param {ColumnsProps} props - The properties object.
+ * @param {Object} props.twoColumns - An object containing two arrays of content blocks.
+ * @param {Array<any>} props.twoColumns.columnOne - The first column of content blocks.
+ * @param {Array<any>} props.twoColumns.columnTwo - The second column of content blocks.
+ *
+ * @returns {JSX.Element} A JSX element containing two columns of content blocks.
+ */
 export const Columns: React.FC<ColumnsProps> = ({ twoColumns: { columnOne, columnTwo }}) => {
     return (
         <div className="flex justify-center items-center">
